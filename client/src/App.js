18577ef70f98be29f8bcc75956b6900b81a3a045
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-20 20:26:05 +0530

    Imported Required Packages in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
+import React, { useEffect, useRef, useState } from "react"
+import { CopyToClipboard } from "react-copy-to-clipboard"
+import Peer from "simple-peer"
+import io from "socket.io-client"
 
+const socket = io.connect("http://localhost:8000")
 
 function App() {
   return (
