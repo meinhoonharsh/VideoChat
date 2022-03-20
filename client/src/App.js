ad40da61a7cddfcda4c4062ac3018b6417fd84a5
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-20 20:40:27 +0530

    Created States and References

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import React, { useEffect, useRef, useState } from "react"
-import { CopyToClipboard } from "react-copy-to-clipboard"
-import Peer from "simple-peer"
-import io from "socket.io-client"
+import React, { useEffect, useRef, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Peer from "simple-peer";
+import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:8000")
+const socket = io.connect("http://localhost:8000");
 
 function App() {
-  return (
-    <div className="App">
-      Jai Hind Dosto....
-    </div>
-  );
+  const [me, setMe] = useState("");
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
+
+  const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
+
+
+  return <div className="App">Jai Hind Dosto....</div>;
 }
 
 export default App;
