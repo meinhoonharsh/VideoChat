8d4ec8016e6a58e0fc75f54f8a7f9f812818e056
meinhoonharsh <harshprogrammer782@gmail.com>
2022-04-10 14:20:58 +0530

    Created Routing in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
+// Import React routing components
+import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Room from "./pages/Room";
+
 function App() {
   return <>
-    Hello Dosto
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/:roomId" element={<Room />} />
+      </Routes>
+    </BrowserRouter>
   </>
 }
 export default App;
