41d7e408cc3b52fd9ccd1978a8c0a3ede4192ac5
meinhoonharsh <harshprogrammer782@gmail.com>
2022-04-12 01:36:54 +0530

    Created Home Page Layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import './Home.scss'
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <div className='create-room'>
+            <div>
+                <h2>Create Your Space</h2>
+                <input type="text" placeholder="Space Name/Id" />
+                <input type="text" placeholder="Your Name" />
+                <button>Create</button>
+            </div>
+        </div>
     )
 }
