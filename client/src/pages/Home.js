681050d1b58b99fcd4284657cd7799794617729f
meinhoonharsh <harshprogrammer782@gmail.com>
2022-04-12 01:45:39 +0530

    Created States of name and spaceId on HomePage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.scss'
 
 export default function Home() {
+    const [name, setName] = useState('')
+    const [spaceId, setSpaceId] = useState('')
     return (
         <div className='create-room'>
             <div>
                 <h2>Create Your Space</h2>
-                <input type="text" placeholder="Space Name/Id" />
-                <input type="text" placeholder="Your Name" />
-                <button>Create</button>
+                <input type="text" placeholder="Space Name/Id" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder="Your Name" value={spaceId} onChange={(e) => setSpaceId(e.target.value)} />
+                <button >Create</button>
             </div>
         </div>
     )
