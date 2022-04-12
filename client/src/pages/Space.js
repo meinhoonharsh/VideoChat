1708d949ae4e5c29f15d30078862c7e8481718d4
meinhoonharsh <harshprogrammer782@gmail.com>
2022-04-12 23:14:23 +0530

    Cleaned Space

--- client/src/pages/Space.js
+++ client/src/pages/Space.js
@@ -6,46 +6,11 @@ import io from "socket.io-client";
 const socket = io.connect("http://localhost:8000");
 
 export default function Space() {
-    const [stream, setStream] = useState();
 
-    navigator.mediaDevices
-        .getUserMedia({ video: true, audio: true })
-        .then((stream) => {
-            // setStream(stream);
-            // if (myVideo.current) {
-            //     myVideo.current.srcObject = stream;
-            // }
-        });
-
-    socket.on("someonecalling", (data) => {
-        console.log("someonecalling", data);
-
-    })
-
-    const createPeer = () => {
-        const peer = new Peer({
-            initiator: true,
-            trickle: false,
-            stream: stream
-        });
-
-        peer.on("signal", (data) => {
-            console.log("signal", data);
-            socket.emit("callsomeone", {
-                signal: data,
-            });
-        });
-
-        peer.on("stream", (stream) => {
-            console.log("peer-on - stream", stream);
-        });
-
-    }
 
     return (
         <>
-            <div>Room</div>
-            <button onClick={createPeer}> Create Peer</button>
+            Your Space
         </>
     )
 }
