3efc3a162f450aa2fdea227ebc3558b2347c6f3b
meinhoonharsh <harshprogrammer782@gmail.com>
2022-04-12 23:17:22 +0530

    Added name taking in SPace

--- client/src/pages/Space.js
+++ client/src/pages/Space.js
@@ -6,11 +6,25 @@ import io from "socket.io-client";
 const socket = io.connect("http://localhost:8000");
 
 export default function Space() {
+    const [name, setName] = useState("");
 
 
     return (
         <>
-            Your Space
+            {!name &&
+                <div className="create-room">
+                    <div>
+                        <h2>Enter this Space</h2>
+                        <input type="text" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+                        <button onClick={() => {
+                            localStorage.setItem("name", name);
+                            socket.emit("joinSpace", { name: name });
+                        }
+                        }>Join</button>
+                    </div>
+                </div>
+            }
+
         </>
     )
 }
